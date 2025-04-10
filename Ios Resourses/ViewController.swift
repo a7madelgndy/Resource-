df55ac436b49921b3081b1f40db3b3cad3467a5c
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2025-04-10 11:31:55 +0200

    Initial Commit

---
+++ Ios Resourses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ios Resourses
+//
+//  Created by Ahmed El Gndy on 10/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
