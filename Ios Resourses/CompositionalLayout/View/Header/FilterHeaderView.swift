5953ea929e804db55529185226e2a4da35bd8e9c
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2025-04-17 14:39:12 +0200

    FilterHeaderView

---
+++ Ios Resourses/CompositionalLayout/View/Header/FilterHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  FilterHeaderView.swift
+//  Ios Resourses
+//
+//  Created by Ahmed El Gndy on 17/04/2025.
+//
+
+import UIKit
+
+class FilterHeaderView: UICollectionReusableView {
+        
+}
