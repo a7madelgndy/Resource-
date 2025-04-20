06a156b022600de68349bc8ae1e3adb94c1103f3
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2025-04-20 13:55:10 +0200

    added coredata to the project

---
+++ Ios Resourses/CoreData/CoredataVC.swift
@@ -0,0 +1,22 @@
+//
+//  CoredataVC.swift
+//  Ios Resourses
+//
+//  Created by Ahmed El Gndy on 20/04/2025.
+//
+
+import UIKit
+
+class CoredataVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+}
+
+Preview {
+    CoredataVC()
+}
