e453072e71e7b03e3aed67555f6777b26084f178
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2025-04-29 13:17:57 +0300

    learned CGD

---
+++ Ios Resourses/CGD/CGDViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CGDViewController.swift
+//  Ios Resourses
+//
+//  Created by Ahmed El Gndy on 29/04/2025.
+//
+
+import UIKit
+
+class CGDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
